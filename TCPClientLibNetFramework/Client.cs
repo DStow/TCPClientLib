b9b3210849c3b1729fca5cc7b36b3327a40a5744
Dan Stow <Dan_Stow@live.co.uk>
2018-06-09 23:48:33 +0100

    Add a NetFramework version of the library

---
+++ TCPClientLibNetFramework/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPClientLibNetFramework
+{
+    public class Client
+    {
+        private string _server = "";
+        private int _port = 0;
+
+        public Client(string server, int port)
+        {
+            _server = server;
+            _port = port;
+        }
+
+        public string SendMessage(string message)
+        {
+            string result = "";
+
+            System.Net.Sockets.TcpClient client = new TcpClient(_server, _port);
+
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            NetworkStream stream = client.GetStream();
+
+            stream.Write(data, 0, data.Length);
+
+            data = new Byte[256];
+
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            result = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            Console.WriteLine("Received: {0}", responseData);
+
+            // Close everything.
+            stream.Close();
+            client.Close();
+
+            return result;
+        }
+    }
+}
